60698844732db1d9a5d5ae275d2f46da7cc4e191
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-11-09 07:40:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -139,7 +139,7 @@
         textStage.update();
 
         var ctx = document.getElementById('text').getContext('2d');
-        var pix = ctx.getImageData(0,0,600,200).data;
+        var pix = ctx.getImageData(0,0,2000,300).data;
         textPixels = [];
         for (var i = pix.length; i >= 0; i -= 4) {
             if (pix[i] != 0) {
